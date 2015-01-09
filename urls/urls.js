4a350ae0c8c0a14ad9f72e16c725cb2b1c1982f0
Elmer Alvarado <elmer.al26@gmail.com>
2015-01-09 16:59:08 -0400

    commit the schedule files

--- urls/urls.js
+++ urls/urls.js
@@ -1,20 +1,32 @@
 var urls = {} || urls;
 
 urls = {
-    functions : {
-        loadUrl : function(app) {
+    functions : 
+    {
+        loadUrl : function(app) 
+        {
             ulrs.loadUrlStudent(app);
             urls.loadUrlTeacher(app);
+            urls.loadUrlSchedule(app)
         },
-        loadUrlStudent : function(app) {
+        loadUrlStudent : function(app) 
+        {
             app.get('/student', function(req, res) {
                 res.render('student/index');
             });
 
         },
-        loadUrlTeacher : function(app) {
+        loadUrlTeacher : function(app) 
+        {
             app.get('/teacher', function(req, res) {
                 res.render('teacher/index');
+            });
+        },
+        loadUrlSchedule : function(app)
+        {
+            app.get('/schedule', function(req, res)
+            {
+                res.render('schedule/index')
             });
         }
     }
