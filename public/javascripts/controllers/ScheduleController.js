175f580d528f5b7855787e0d39f22c7c75b44cf9
luis fernando camacho camacho <luis.fernando.c.c.1987@gmail.com>
2015-01-25 14:23:45 -0800

    center calendar inside template

--- public/javascripts/controllers/ScheduleController.js
+++ public/javascripts/controllers/ScheduleController.js
@@ -10,7 +10,7 @@ ColegioEPA.ScheduleController = Ember.ArrayController.extend({
 });
 
 window.ColegioEPA.ApplicationController = Ember.Controller.extend
-(
+({
   needs: ['calendar']
 });
 
@@ -50,75 +50,6 @@ ColegioEPA.CalendarController = Ember.Calendar.CalendarController.extend
 
     events.push(event);
   }
-
-  date = 2;
-  time = 1000 * 60 * 60 * 12;
-  duration = 1000 * 60 * 60 * 2;
-  eventName = 'Pending Piano Class';
-  eventLocation = 'B1';
-  eventType = 0;
-
-  event =
-{
-  name: eventName + events.length
-  , start: moment().startOf('day').add('days', date - moment().day()).add('milliseconds', time)
-  , end: moment().startOf('day').add('days', date - moment().day()).add('milliseconds', time + duration)
-  , location: eventLocation
-  , type: eventType
-
-
-};
-
-events.push(event);
-
-date = 3;
-time = 1000 * 60 * 60 * 10 ;
-duration = 1000 * 60 * 60 * 2;
-eventName = 'Going Guitar Class';
-eventType = 1;
-
-event =
-{
-  name: eventName + events.length
-  , start: moment().startOf('day').add('days', date - moment().day()).add('milliseconds', time)
-  , end: moment().startOf('day').add('days', date - moment().day()).add('milliseconds', time + duration)
-  , type: eventType
-};
-
-events.push(event);
-
-date = 5;
-time = 1000 * 60 * 60 * 11 ;
-duration = 1000 * 60 * 60 * 2;
-eventName = 'Done Sing Class';
-eventType = 2;
-
-event =
-{
-  name: eventName + events.length
-  , start: moment().startOf('day').add('days', date - moment().day()).add('milliseconds', time)
-  , end: moment().startOf('day').add('days', date - moment().day()).add('milliseconds', time + duration)
-  , type: eventType
-};
-
-events.push(event);
-
-date = 6;
-time = 1000 * 60 * 60 * 13 ;
-duration = 1000 * 60 * 60 * 2;
-eventName = 'suspended Sing Class';
-eventType = 3;
-
-event =
-{
-  name: eventName + events.length
-  , start: moment().startOf('day').add('days', date - moment().day()).add('milliseconds', time)
-  , end: moment().startOf('day').add('days', date - moment().day()).add('milliseconds', time + duration)
-  , type: eventType
-};
-
-events.push(event);
-
 return events;
 }.property(),
 
--- public/javascripts/routes/StudentRoute.js
+++ public/javascripts/routes/StudentRoute.js
@@ -5,28 +5,6 @@ ColegioEPA.Router.map(function () {
       this.route("edit", {path:"/:student_id"});
     });
 });
-
-<<<<<<< HEAD
-    ColegioEPA.StudentsIndexRoute = Ember.Route.extend({
-        setupController: function(controller) {
-            var students = this.get('store').find('student'); // App.Location.find();
-            controller.set('content', students);
-        },
-        renderTemplate: function() {
-          this.render('students.index',{into:'application'});
-        }
-    });
-
-    ColegioEPA.StudentsNewRoute = Ember.Route.extend({
-      setupController: function(controller, model) {
-        var newStudent = this.store.createRecord('student', {});
-        this.controllerFor('students.edit').setProperties({isNew: true, content:newStudent});
-      },
-      renderTemplate: function() {
-        this.render('students.edit', {into:'application'})
-      }
-    });
-=======
 ColegioEPA.StudentsIndexRoute = ColegioEPA.AuthenticatedRoute.extend({
     setupController: function(controller) {
         var students = this.get('store').find('student'); // App.Location.find();
@@ -46,7 +24,6 @@ ColegioEPA.StudentsNewRoute = ColegioEPA.AuthenticatedRoute.extend({
     this.render('students.edit', {into:'application'})
     }
 });
->>>>>>> db2b169ae470e02bfaf8f2c26eeed98727335125
 
 ColegioEPA.StudentsEditRoute = ColegioEPA.AuthenticatedRoute.extend({
     setupController: function(controller, model) {
