45b89f28b492efc10dc78248c24722d2c110143f
luis fernando camacho camacho <luis.fernando.c.c.1987@gmail.com>
2015-01-25 16:15:06 -0800

    calendar 1st version

--- public/javascripts/controllers/ScheduleController.js
+++ public/javascripts/controllers/ScheduleController.js
@@ -13,6 +13,8 @@ window.ColegioEPA.ApplicationController = Ember.Controller.extend
 ({
   needs: ['calendar']
 });
+ColegioEPA.couresValues =[];
+
 
 ///////////////////////////////////////////////////////////////////////////////
 // Controller
@@ -20,38 +22,63 @@ window.ColegioEPA.ApplicationController = Ember.Controller.extend
 ColegioEPA.CalendarController = Ember.Calendar.CalendarController.extend
 ({
   needs: "Schedule",
-  content: function (){
-  var courses = this.store.findAll('course');
-  var events = [];
+  content: [],
+   update : function() {
+    var self = this;
+    var events = [];
+  var getCourses = $.ajax({
+    url:'http://localhost:3000/api/course/courses',
+    type:'GET',
+    dataType: 'json',
+    contentType: "application/json; charset=utf-8",
+    headers : {'API_KEY': localStorage.getItem("token")},
+    success:function(result){
+        result.courses.forEach(function(course) {
+            date = events.length;
+            time = 1000 * 60 * 60 * 10;
+            duration = 1000 * 60 * 60 * 2;
+            eventName = course.name;
+            eventLocation = 'B1';
+            eventType = 1;
+            event =
+                {
+                  name: eventName + events.length
+                  , start: moment().startOf('day').add('days', date - moment().day()).add('milliseconds', time)
+                  , end: moment().startOf('day').add('days', date - moment().day()).add('milliseconds', time + duration)
+                  , location: eventLocation
+                  , type: eventType
+
+                };
+            events.push(event);
+            event=
+                {
+                  name: eventName + events.length
+                  , start: moment().startOf('day').add('days', date - moment().day()).add('milliseconds', time)
+                  , end: moment().startOf('day').add('days', date - moment().day()).add('milliseconds', time + duration)
+                  , location: eventLocation
+                  , type: eventType
+
+                };
+
+            events.push(event);
+
+        });
+         self.clear().pushObjects(events).notifyPropertyChange('content');
+    },
+    error:function(res){
+
+        alert("Bad thing happend! " + res.statusText);
+      }
+    });
+
   var date;
   var time;
   var duration;
   var eventName;
   var event;
 
-  for (var course in courses) {
-
-    date = 1;
-    time = 1000 * 60 * 60 * 10;
-    duration = 1000 * 60 * 60 * 2;
-    eventName = course.cost;
-    eventLocation = 'B1';
-    eventType = 1;
-
-    event =
-    {
-      name: eventName + events.length
-      , start: moment().startOf('day').add('days', date - moment().day()).add('milliseconds', time)
-      , end: moment().startOf('day').add('days', date - moment().day()).add('milliseconds', time + duration)
-      , location: eventLocation
-      , type: eventType
-
-    };
-
-    events.push(event);
-  }
 return events;
-}.property(),
+}.observes('week'),
 
  eventViewClass: 'ColegioEPA.EventView'
 });
