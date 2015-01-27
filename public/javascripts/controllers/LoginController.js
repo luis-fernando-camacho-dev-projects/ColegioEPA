084b55e03a05a33191ee234c9b592cd72e11fd8a
luis fernando camacho camacho <luis.fernando.c.c.1987@gmail.com>
2015-01-27 00:02:04 -0800

    adding option to retrieve information user and implement option logout

--- public/javascripts/controllers/LoginController.js
+++ public/javascripts/controllers/LoginController.js
@@ -20,21 +20,25 @@ ColegioEPA.LoginIndexController = Ember.Controller.extend({
     // Clear out any error messages.
     this.set('errorMessage', null);
         $.post('/login', data).done(function(response) {
-          self.set('token', response.token);
-          var attemptedUrlValue = localStorage.getItem("attemptedUrl");
-            if (attemptedUrlValue !== null && attemptedUrlValue) {
-              delete localStorage.attemptedUrl;
+        self.set('token', response.token);
+        var attemptedUrlValue = localStorage.getItem("attemptedUrl");
+            localStorage.setItem('user', JSON.stringify(response));
+            localStorage.setItem('role', response.role);
+
+        if (attemptedUrlValue !== null && attemptedUrlValue) {
               window.location.href = attemptedUrlValue;
-            } else if (response.role === 'student') {
-                window.location.href = "http://localhost:3000/student/attendance";
-            } else if (response.role === 'administrator') {
-                window.location.href = "http://localhost:3000/teacher";
-            } else if (response.role === 'teacher') {
-                window.location.href = "http://localhost:3000/teacher/attendance";
-            }
-        })
-        .fail(function(response) {
+        } else if (response.role === 'student') {
+            localStorage.setItem('_objectId', response.student);
+            window.location.href = "http://localhost:3000/student/attendance";
+        } else if (response.role === 'administrator') {
+            localStorage.setItem('_objectId', response.teacher);
+            window.location.href = "http://localhost:3000/teacher";
+        } else if (response.role === 'teacher') {
+            localStorage.setItem('_objectId', response.teacher);
+            window.location.href = "http://localhost:3000/teacher/attendance";
+        }
+    }).fail(function(response) {
             alert("error");
-        });
-  }
+    });
+    }
 });
